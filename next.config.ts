f93f37229c7f1fce676734dad9aff818c543717e
Mrid22 <mridulaga@outlook.com>
2025-05-17 00:13:00 +0800

    trying to fix asset paths

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
   basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
